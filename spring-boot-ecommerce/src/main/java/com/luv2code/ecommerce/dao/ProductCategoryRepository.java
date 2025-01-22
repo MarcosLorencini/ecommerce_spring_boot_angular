aa4d55e1cbc03fdcc8d2099502c9e41adf1fd605
MarcosLorencini <mlorencinif@gmail.com>
2025-01-22 14:07:34 -0300

    crição do respositórios Product e ProductCategory

---
+++ spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductCategoryRepository.java
@@ -0,0 +1,20 @@
+package com.luv2code.ecommerce.dao;
+
+import com.luv2code.ecommerce.entity.ProductCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+//gerar automaticamente uma API REST sem a necessidade de implementar controladores (controllers) explicitamente.
+/*Após executar a aplicação, os seguintes endpoints estarão disponíveis automaticamente:
+
+        GET /product-category: Retorna todos as categorias de produtos.
+        GET /product-category/{id}: Retorna uma categoria de produtos.
+        POST /product-category: Cria um nova categoria de produtos.
+        PUT /product-category/{id}: Atualiza uma categoria de produtos.
+        DELETE /product-category/{id}: Exclui uma categoria de produtos.
+
+        collectionResourceRel nome do Json de entrada
+        */
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+}
